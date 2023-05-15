b8c9a1b28b442e70bb719005801e42406323bd93
LuckyBelieve <luckybelieve10@gmail.com>
2023-05-15 22:10:48 +0200

    another commit

--- Frontend/src/screens/signInScreen.jsx
+++ Frontend/src/screens/signInScreen.jsx
@@ -1,21 +1,38 @@
 import {Button, Container,Form, FormControl, FormGroup, FormLabel} from "react-bootstrap";
 import {Helmet} from "react-helmet-async";
 import {Link,useLocation} from "react-router-dom";
-
+import axios from "axios";
+import {burl} from "../utils/url";
+import { useState } from "react";
+import {getError} from "../utils/getError";
 const SignInScreen = () => {
     const {search} = useLocation();
     const redirectURL = new URLSearchParams(search).get('redirect');
     const redirect = redirectURL?redirectURL:"/";
+    const [email,setEmail] = useState("");
+    const [password,setpassword] = useState("");
+    const hundleFormSubmit = async (e)=>{
+        e.preventDefault();
+        try {
+            const {data} = await axios.post(burl+"/api/users/signin",{
+              email,
+              password
+            });
+            localStorage.getItem("userInfo",JSON.stringify(data));   
+        } catch (err) {
+            getError(err);
+        }
+    }
     return (
         <Container className="small-container">
             <Helmet>
                 <title>Sign In</title>
             </Helmet>
             <h1 className=" my-3">sign In</h1>
-            <Form>
+            <Form onSubmit={hundleFormSubmit}>
                 <FormGroup className="mb-3" controlId="email">
                 <FormLabel>Email</FormLabel>
-                <FormControl type="email" required />
+                <FormControl type="email" required onChange={(e)=>setEmail(e.target.value)}/>
                 </FormGroup>
                 <FormGroup className="mb-3" controlId="email">
                 <FormLabel>password</FormLabel>
